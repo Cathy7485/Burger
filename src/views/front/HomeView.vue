dfe59944684e6a87f2e72b90455163a90aa98b59
cathy <cathy141153@gmail.com>
2023-03-02 15:24:55 +0800

    update

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -1,13 +1,41 @@
 <template>
-  <div class="home">這是首頁</div>
+	<div class="container">
+		<div class="row">
+			<div class="block-title">
+				<div class="title">暢銷漢堡</div>
+			</div>
+			<!-- swiper -->
+			<div class="col-6"></div>
+			<div class="col-6"></div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="block-title">
+				<div class="title">品牌故事</div>
+			</div>
+			<div class="col-6">
+				<img src="" alt="">
+			</div>
+			<div class="col-6"></div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="block-title">
+				<div class="title">客戶評價</div>
+			</div>
+			<div class="col-lg-4"></div>
+			<div class="col-lg-4"></div>
+			<div class="col-lg-4"></div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="block-title">
+				<div class="title">地理位置</div>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
-export default{
-  components:{}
-}
-</script>
-
-<style scoped>
-
-</style>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-	// base: '/C-burger/',
+	base: '/burger/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
