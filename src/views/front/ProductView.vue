78ca3b530b622cf8a33368e690c1fd271ccaac3c
cathy <cathy141153@gmail.com>
2023-03-13 21:07:02 +0800

    add product page

--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -1,12 +1,39 @@
 <template>
   <PageBanner :pageTitle="pageTitle"></PageBanner>
   <section id="page-content" class="container">
-    
-    <h2>{{ product.title }}</h2>
-    <img :src="product.imageUrl" class="w-50" alt="" />
-    <button class="btn btn-primary" type="button"
-      @click="() => addToCart(product.id)"
-    >加入購物車</button>
+    <div id="product-page" class="row">
+      <div class="col-lg-6">
+        <div class="img">
+          <img :src="product.imageUrl" :alt="product.title" />
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="info">
+          <span class="badge text-bg-success">{{ product.category }}</span>
+          <div class="title">{{ product.title }}</div>
+          <hr>
+          <div class="description">
+            <div class="d-flex flex-row">
+              <div class="d-block me-2" style="min-width: 90px;white-space: nowrap;">產品描述：</div>
+              <div class="d-block">{{ product.description }}</div>
+            </div>
+          </div>
+          <div class="content">
+            <div class="d-flex flex-row">
+              <div class="d-block me-2" style="min-width: 90px;white-space: nowrap;">產品成分：</div>
+              <div class="d-block">{{ product.content }}</div>
+            </div>
+          </div>
+          <button
+            class="btn btn-primary"
+            type="button"
+            @click="() => addToCart(product.id)"
+          >
+            加入購物車
+          </button>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -32,21 +59,21 @@ export default {
       const loader = this.$loading.show({
         container: this.fullPage ? null : this.$refs.formContainer, //不要滿版，需要設定一個vi-parent，只loading該區塊
         canCancel: false,
-        onCancel: this.onCancel
-      })
+        onCancel: this.onCancel,
+      });
       const { id } = this.$route.params;
       this.$http
         .get(`${VITE_URL}api/${VITE_PATH}/product/${id}`)
         .then((res) => {
           this.product = res.data.product;
           this.isLoading = false;
-          loader.hide()
+          loader.hide();
         })
         .catch((err) => {
           console.log(err.response.data.message);
         });
     },
-    ...mapActions(cartStore,['addToCart']),
+    ...mapActions(cartStore, ["addToCart"]),
   },
   mounted() {
     this.getProduct();
