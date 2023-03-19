a9595141b3633fe0cd3e8792b2a027e412a23067
cathy <cathy141153@gmail.com>
2023-03-19 20:01:08 +0800

    update order-page

--- src/views/front/OrderList.vue
+++ src/views/front/OrderList.vue
@@ -1,6 +1,16 @@
 <template>
   <PageBanner :pageTitle="pageTitle"></PageBanner>
-  <section id="page-content" class="container">尚未建置</section>
+  <section id="page-content" class="container">
+    <div id="checkOrder" class="row">
+      <div class="col-lg-8 m-auto">
+        <div class="form">
+          <label for="idNum" class="form-label">訂單編號：</label>
+          <input type="text" id="idNum" class="form-control" placeholder="請輸入訂單編號">
+          <button type="button" class="btn btn-primary">送出</button>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
