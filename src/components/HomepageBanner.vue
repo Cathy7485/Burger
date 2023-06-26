6cb37f0eb95d1bdc131c52eebae6dad3f6ad550e
cathy <cathy141153@gmail.com>
2023-06-26 08:39:42 +0800

    update banner

--- src/components/HomepageBanner.vue
+++ src/components/HomepageBanner.vue
@@ -1,8 +1,9 @@
 <template>
 	<section id="banner">
-    <img src="../assets/banner.jpg" alt="banner" />
+    <img src="../assets/banner.png" alt="banner" />
+		<img src="../assets/banner-burger.png" class="burger-img" alt="banner-burger" />
 		<div class="banner-title" data-aos="fade-up" data-aos-once="true" data-aos-duration="400">
-      <span>來吃堡，吃飽飽</span> 
+      <span>吃堡</span> 
       <span class="subtitle">我們的漢堡都是手工製作！健康美味無負擔！</span>
     </div>
   </section>
