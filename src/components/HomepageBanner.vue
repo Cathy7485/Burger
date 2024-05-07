6779afdae972f204e67205d796fcb23630b06f87
Cathy L <cathy141153@gmail.com>
2024-05-07 22:10:38 +0800

    feat:調整圖片資料夾及圖片路徑

--- src/components/HomepageBanner.vue
+++ src/components/HomepageBanner.vue
@@ -1,10 +1,10 @@
 <template>
-	<section id="banner">
-		<img src="../assets/banner.png" alt="banner" />
-		<img src="../assets/banner-burger.png" class="burger-img" alt="banner-burger" />
+	<section class="banner">
+		<img src="../assets/images/banner-text.png" class="banner-text" alt="banner-text" />
+		<img src="../assets/images/banner-burger.png" class="burger-img" alt="banner-burger" />
 		<div class="banner-title" data-aos="fade-up" data-aos-once="true" data-aos-duration="400">
 			<span>吃堡</span>
-			<span class="subtitle">我們的漢堡都是手工製作！健康美味無負擔！</span>
+			<span class="sub-title">我們的漢堡都是手工製作！健康美味無負擔！</span>
 		</div>
 	</section>
 </template>
