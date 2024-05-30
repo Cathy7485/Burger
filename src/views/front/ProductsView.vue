caf84c53442511c3bd1735d926314bbf595cefed
Cathy L <cathy141153@gmail.com>
2024-05-30 21:59:56 +0800

    feat:修改樣式

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -3,7 +3,7 @@
 	<section id="page-content" class="container vl-parent" ref="formContainer">
 		<ul id="product-list" class="row">
 			<li 
-				class="product-item col-lg-4 col-md-6" 
+				class="product-page-item col-lg-4 col-md-6" 
 				v-for="product in products" 
 				:key="product.id">
 				<div class="img">
